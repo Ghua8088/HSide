0d03e1cdbe747de32e78c45540cbac8d62f349e1
Ghua8088 <137153710+Ghua8088@users.noreply.github.com>
2025-06-26 20:23:16 +0530

    1.0.2 release

--- src/ide/AIBridge.java
+++ src/ide/AIBridge.java
@@ -2,7 +2,6 @@ package ide;
 
 public class AIBridge {
     private static final AIClient instance = new AIClient(); // or lazy init
-
     public static AIClient getInstance() {
         return instance;
     }
